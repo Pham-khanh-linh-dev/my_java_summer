71fcc5adb01b133323353691e809453be6f45685
Pham-khanh-linh-dev <khanhbl9848@gmail.com>
2024-06-19 19:37:15 +0700

    init project

---
+++ src/Hello_World/Helloworld.java
@@ -0,0 +1,11 @@
+package Hello_World;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java:");
+		System.out.println("Hello world with java:");
+		System.out.println("Hello world with java:");
+		System.out.println("Hello world with java:");
+		System.out.println("Hello world with java:");
+	}
+}
